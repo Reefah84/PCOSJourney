e3011c5dc4d79e47f746b4df381d1f2c443e2a93
Reefah84 <202214084@student.mist.ac.bd>
2023-11-17 11:10:30 +0600

    I woke up to see all of them gone

--- src/main/java/com/example/pcos_journey/dashboardDoctorController.java
+++ src/main/java/com/example/pcos_journey/dashboardDoctorController.java
@@ -1,32 +1,121 @@
 package com.example.pcos_journey;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
 import javafx.scene.control.MenuButton;
-import javafx.scene.layout.VBox;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 
+import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.stream.Collectors;
 
 public class dashboardDoctorController {
 
     public Button logout;
     public MenuButton healthbutton;
-    public MenuButton alldoctor;
-    public Button news;
     public Label welcome;
-    public VBox notification;
-
+    public ListView<String>patientMessage;
+    public ListView<String> myReply;
+    public Button gy;
+    public ImageView home;
+    private String doctoremail;
     public void initialize() {
-        // Access the logged-in user from UserSession
         User loggedInUser = UserSession.getLoggedInUser();
-
         if (loggedInUser != null) {
-            // Display the username in the usernameLabel
-            welcome.setText("Welcome Dr" + loggedInUser.getUsername());
+            welcome.setText("Welcome Dr " + loggedInUser.getUsername());
+            doctoremail = loggedInUser.getUsername();
             welcome.setVisible(true);
+        }
+
+        loadpatientMessage();
+        loadReplies();
+        setupListViewListeners();
+    }
+
+    private void loadpatientMessage() {
+        String doctorDirectoryPath = "E:/Java/PCOS_Journey/src/main/java/com/example/pcos_journey/DRData/" + doctoremail;
+        File doctorDirectory = new File(doctorDirectoryPath);
+
+        String[] patientMessages = doctorDirectory.list((dir, name) -> name.startsWith("message_from_"));
+        if (patientMessages != null) {
+            patientMessage.getItems().addAll(
+                    Arrays.stream(patientMessages)
+                            .map(this::formatFileName)
+                            .collect(Collectors.toList())
+            );
+        }
+    }
+
+    private void loadReplies() {
+        String doctorDirectoryPath = "E:/Java/PCOS_Journey/src/main/java/com/example/pcos_journey/DRData/" + doctoremail;
+        File doctorDirectory = new File(doctorDirectoryPath);
+
+        String[] replies = doctorDirectory.list((dir, name) -> name.startsWith("replied_to_"));
+        if (replies != null) {
+            myReply.getItems().addAll(
+                    Arrays.stream(replies)
+                            .map(this::formatFileName)
+                            .collect(Collectors.toList())
+            );
+        }
+    }
+
+    private String formatFileName(String fileName) {
+        return fileName.replace("message_from_", "Message from ")
+                .replace("replied_to_", "Replied to ")
+                .replace("_", " ")
+                .replace(".txt", "");
+    }
+
+    private void setupListViewListeners() {
+        patientMessage.setOnMouseClicked(event -> {
+            if (event.getClickCount() == 2) {
+                String selectedMessage = patientMessage.getSelectionModel().getSelectedItem();
+                if (selectedMessage != null) {
+                    openPatientMessagePopup(selectedMessage, false);
+                }
+            }
+        });
+
+        myReply.setOnMouseClicked(event -> {
+            if (event.getClickCount() == 2) {
+                String selectedReply = myReply.getSelectionModel().getSelectedItem();
+                if (selectedReply != null) {
+                    openPatientMessagePopup(selectedReply, true);
+                }
+            }
+        });
+    }
+
+    private void openPatientMessagePopup(String fileName, boolean isReply) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("SeePatientMessage.fxml"));
+            Parent root = loader.load();
+
+            SeePatientMessage controller = loader.getController();
+            String filePrefix = isReply ? "replied_to_" : "message_from_";
+            String filePath = "E:/Java/PCOS_Journey/src/main/java/com/example/pcos_journey/DRData/" + doctoremail + "/" + fileName.replace(isReply ? "Replied to " : "Message from ", filePrefix).replace(" ", "_") + ".txt";
+            String messageContent = new String(Files.readAllBytes(Paths.get(filePath)));
+            controller.setMessage(messageContent);
+
+            Stage stage = new Stage();
+            stage.initModality(Modality.APPLICATION_MODAL);
+            stage.setTitle("Patient Message");
+            stage.setScene(new Scene(root));
+            stage.showAndWait();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
     public void setLogout(ActionEvent event)
@@ -43,6 +132,39 @@ public class dashboardDoctorController {
         stage.setResizable(false);
         stage.setScene(scene);
         stage.show();
+    }
+    public void setBackHome(MouseEvent event) {
+        try {
+            // Load the new FXML file
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("homepage_attempt2.fxml"));
+            Parent root = loader.load();
+
+            // Get the current stage
+            Stage stage = (Stage) home.getScene().getWindow();
+
+            // Set the new content in the same window
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void showListOfDoctors(ActionEvent event) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("ListofDoctors.fxml"));
+            Parent root = loader.load();
+
+            // Get the current stage
+            Stage stage = (Stage) gy.getScene().getWindow();
+
+            // Set the new content in the same window
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+        } catch (IOException e) {
+            e.printStackTrace();
+            // Handle the exception
+        }
     }
 
 }
--- src/main/java/com/example/pcos_journey/dashboardUserController.java
+++ src/main/java/com/example/pcos_journey/dashboardUserController.java
@@ -7,7 +7,8 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.MenuButton;
-import javafx.scene.layout.VBox;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -17,13 +18,13 @@ public class dashboardUserController {
 
     public Button logout;
     public MenuButton healthbutton;
-    public MenuButton alldoctor;
-    public Button news;
+
     public Label welcome;
     public Button symptomButton;
     public Button weightButton;
     public Button DoctorButton;
-    public VBox notification;
+    public Button gy;
+    public ImageView home;
 
     public void initialize() {
         // Access the logged-in user from UserSession
@@ -58,6 +59,39 @@ public class dashboardUserController {
             Scene scene = new Scene(root);
             // If you have a stylesheet
             scene.getStylesheets().add(Objects.requireNonNull(getClass().getResource("style.css")).toExternalForm());
+            stage.setScene(scene);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void setWeightButton(ActionEvent event) {
+        try {
+            // Load the new FXML file
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("WeightTracker.fxml"));
+            Parent root = loader.load();
+
+            // Get the current stage
+            Stage stage = (Stage) weightButton.getScene().getWindow();
+
+            // Set the new content in the same window
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public void setHome(MouseEvent event) {
+        try {
+            // Load the new FXML file
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("homepage_attempt2.fxml"));
+            Parent root = loader.load();
+
+            // Get the current stage
+            Stage stage = (Stage) home.getScene().getWindow();
+
+            // Set the new content in the same window
+            Scene scene = new Scene(root);
             stage.setScene(scene);
         } catch (IOException e) {
             e.printStackTrace();
